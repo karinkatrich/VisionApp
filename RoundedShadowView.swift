19c38a1d94b52dc956c06debcd83b19d4afbcc9d
karinkatrich <karinkatrich@gmail.com>
2017-12-15 14:22:44 +0100

    missed file added

--- RoundedShadowView.swift
+++ RoundedShadowView.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class RoundedShadowView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowRadius = 15
+        self.layer.shadowOpacity = 0.75
+        self.layer.cornerRadius = self.frame.height / 2
     }
-    */
+
 
 }
--- vision-app/View/RoundedShadowButton.swift
+++ vision-app/View/RoundedShadowButton.swift
@@ -10,12 +10,11 @@ import UIKit
 
 class RoundedShadowButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowRadius = 15
+        self.layer.shadowOpacity = 0.75
+        self.layer.cornerRadius = self.frame.height / 2
     }
-    */
 
 }
